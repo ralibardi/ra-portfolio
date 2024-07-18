b0629961c06e9a6319f8e4eaf9e91de27fbcfa70
Ronny Alibardi <ronny@alibardi.dev>
2024-07-18 23:23:56 +0100

    chore: update build and test steps in GitHub workflows, fixes for mobile

--- src/components/header/components/header.tsx
+++ src/components/header/components/header.tsx
@@ -9,14 +9,16 @@ import styles from '../assets/header.module.scss';
 const Header: FunctionComponent = () => {
   return (
     <header className={styles.container}>
-      <div className={styles.subContainer}>
-        <CompanyInfo />
-      </div>
-      <div className={styles.subContainer}>
+      <div className={styles.topbar}>
         <Topbar />
       </div>
       <div className={styles.subContainer}>
-        <ThemeToggle />
+        <div className={styles.themeToggle}>
+          <ThemeToggle />
+        </div>
+        <div className={styles.companyInfo}>
+          <CompanyInfo />
+        </div>
       </div>
     </header>
   );
