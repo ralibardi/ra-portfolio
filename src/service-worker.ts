c1091e6c3e87f2ab00f81f0ecf61e9442e79d0e4
Ronny Alibardi <ronny@alibardi.dev>
2024-07-21 19:09:01 +0100

    chore: update index.html to include apple-touch-icon.png

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -25,7 +25,6 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = /\/[^/?]+\.[^/]+$/;
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }: { request: Request; url: URL }) => {
@@ -36,12 +35,6 @@ registerRoute(
 
     // If this is a URL that starts with /_, skip.
     if (url.pathname.startsWith('/_')) {
-      return false;
-    }
-
-    // If this looks like a URL for a resource, because it contains
-    // a file extension, skip.
-    if (fileExtensionRegexp.exec(url.pathname)) {
       return false;
     }
 
