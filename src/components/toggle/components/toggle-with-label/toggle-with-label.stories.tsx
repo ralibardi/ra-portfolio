5a52dd3b1382ff5aea77bc42a9a07eee8e5486d3
Ronny Alibardi <ronny.alibardi@outlook.com>
2024-09-03 19:53:36 +0100

    refactor: update toggle-with-label story title to 'Toggles/ToggleWithLabel'

--- src/components/toggle/components/toggle-with-label/toggle-with-label.stories.tsx
+++ src/components/toggle/components/toggle-with-label/toggle-with-label.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import ToggleWithLabel from './toggle-with-label';
 
 const meta: Meta<typeof ToggleWithLabel> = {
-  title: 'Toggles/ToggleWithIcons',
+  title: 'Toggles/ToggleWithLabel',
   component: ToggleWithLabel,
 };
 
