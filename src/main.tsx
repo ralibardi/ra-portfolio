0696961ea8d58d8b39eeabfa3d1c90e58cde2e5d
Ronny Alibardi <ronny.alibardi@outlook.com>
2024-09-03 14:14:40 +0100

    refactor: update PWAPrompt appIconPath to use relative path

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <ErrorBoundary>
         <ThemeProvider>
-          <PWAPrompt appIconPath="/public/favicon.ico" />
+          <PWAPrompt appIconPath="public/favicon.ico" />
           <App />
         </ThemeProvider>
       </ErrorBoundary>
